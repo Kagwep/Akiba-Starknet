a80a80cf6226d606d8e16d0583d84557215dec73
Peter Kagwe <kagwepeter07@gmail.com>
2023-10-06 03:43:29 +0300

    frontend update

--- web_frontend/src/main.jsx
+++ web_frontend/src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { Suspense } from 'react'
+import ReactDOM from 'react-dom'
+import './assets/css/tailwind.css'
+import App from './App'
+import { SidebarProvider } from './context/SidebarContext'
+import ThemedSuspense from './components/ThemedSuspense'
+import { Windmill } from '@windmill/react-ui'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.render(
+  <SidebarProvider>
+    <Suspense fallback={<ThemedSuspense />}>
+      <Windmill usePreferences>
+        <App />
+      </Windmill>
+    </Suspense>
+  </SidebarProvider>,
+  document.getElementById('root')
 )
